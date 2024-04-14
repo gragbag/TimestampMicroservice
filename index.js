3fb1538e2f5ce5c0fa70c31db484a33a23d51201
julian fong <julianF1725@gmail.com>
2024-04-14 20:58:37 +0000

    Initial commit

--- index.js
+++ index.js
@@ -30,3 +30,28 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/api/:date?', (req, res, next) => {
+  if (isValidDate(req.params.date)) {
+    req.time = new Date(req.params.date);
+  } else if(!isNaN(req.params.date) && Number.parseInt(req.params.date) > 0) {
+    req.time = new Date(Number.parseInt(req.params.date));
+  } else if(!req.params.date) {
+    req.time = new Date()
+  } else {
+    res.send({
+      "error": "Invalid Date"
+    })
+    return;
+  }
+  next();
+}, (req, res) => {
+    res.send({
+      "unix": req.time.getTime(),
+      "utc": req.time.toGMTString()
+    })
+})
+
+const isValidDate = (date) => {
+  return !isNaN(new Date(date));
+}
